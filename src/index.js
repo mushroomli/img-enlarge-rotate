9897ff68cf31085173aa96101b66fe2080d22075
lijiamiao <15733267361@163.com>
2020-10-19 17:02:08 +0800

    修改文件下载名称 Signed-off-by: lijiamiao <15733267361@163.com>

--- src/index.js
+++ src/index.js
@@ -221,6 +221,7 @@ export default class ImgEnlargeAndRotate extends Component {
   }
 
 
+  //下载图片版本1
   saveImg = (url, name) => {
     let browser = myBrowser();
     let odownLoad = document.getElementById('oDownload');
@@ -238,6 +239,43 @@ export default class ImgEnlargeAndRotate extends Component {
       odownLoad.href = url;
       odownLoad.download = "";
     }
+  }
+
+  //下载图片2
+  downloadFile = (content, fileName) => {
+    if (content.indexOf('http') > -1) {
+      let aLink = document.createElement('a');
+      let evt = document.createEvent("HTMLEvents");
+      evt.initEvent("click", true, true); //initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
+      aLink.download = fileName;
+      aLink.href = content;
+      aLink.click();
+    } else {
+      //下载base64图片
+      let base64ToBlob = function (code) {
+        let parts = code.split(';base64,');
+        let contentType = parts[0].split(':')[1];
+        let raw = window.atob(parts[1]);
+        let rawLength = raw.length;
+        let uInt8Array = new Uint8Array(rawLength);
+        for (let i = 0; i < rawLength; ++i) {
+          uInt8Array[i] = raw.charCodeAt(i);
+        }
+        return new Blob([uInt8Array], {
+          type: contentType
+        });
+      };
+
+      let aLink = document.createElement('a');
+      let blob = base64ToBlob(content); //new Blob([content]);
+      let evt = document.createEvent("HTMLEvents");
+      evt.initEvent("click", true, true); //initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
+      aLink.download = fileName;
+      aLink.href = URL.createObjectURL(blob);
+      aLink.click();
+    }
+
+
   }
 
 
@@ -286,7 +324,7 @@ export default class ImgEnlargeAndRotate extends Component {
               hideDownload ? null : <a id='oDownload'>
                 <i className={`iconfont im-xiazai ${cssStyle.toolBtn}`}
                    title='下载图片到本地'
-                   onClick={() => this.saveImg(maxImg, imgName)}/>
+                   onClick={() => this.downloadFile(maxImg, imgName)}/>
               </a>
             }
 
@@ -358,7 +396,7 @@ export default class ImgEnlargeAndRotate extends Component {
               hideDownload ? null : <a id='oDownload'>
                 <i className={`iconfont im-xiazai ${cssStyle.toolBtn}`}
                    title='下载图片到本地'
-                   onClick={() => this.saveImg(maxImg, imgName)}/>
+                   onClick={() => this.downloadFile(maxImg, imgName)}/>
               </a>
             }
 
